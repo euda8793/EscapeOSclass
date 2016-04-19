15cd5cb03d5b367953bd11fa0ea10851a19362ba
Elliot Eudaley <Elliot.M.Eudaley-1@ou.edu>
2016-04-19 12:25:06 -0500

    added animateText() method
    Use the animateText() method to add text to the TextField below the
    inventory panel

--- src/ui/GameWindow.java
+++ src/ui/GameWindow.java
@@ -7,6 +7,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -21,22 +23,26 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.Timer;
 import javax.swing.border.TitledBorder;
 
 @SuppressWarnings("serial")
 public class GameWindow extends JFrame{
 	
-	JPanel pan1;
-	JPanel pan2;
-	JPanel pan3;
-	JPanel pan4;
-	JTextField tf;
+	protected JPanel pan1;
+	protected JPanel pan2;
+	protected JPanel pan3;
+	protected JPanel pan4;
+	protected JTextField tf;
+	
+	private int charIndex;
 	
 	//custom label
 	ILabel lab1;
 	
-	
 	public GameWindow(String title){
+		
+		
 		
 		//JFrame layout
 		super(title);
@@ -51,18 +57,18 @@ public class GameWindow extends JFrame{
 		pan4 = new JPanel();
 		
 		
-		//Label 
+		//Panels set upt
 		lab1 = new ILabel();
 		pan2.add(lab1);
 		pan2.setBackground(Color.BLACK);
 		pan1.add(pan2);
 		pan3.setLayout(new FlowLayout(FlowLayout.LEFT));
 		pan3.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLoweredSoftBevelBorder(), "Inventory", TitledBorder.CENTER, TitledBorder.CENTER,pan3.getFont(), Color.YELLOW));
-		pan3.add(new IButton("PrisonBG.jpg"));
 		pan1.add(pan3);
 		pan3.setPreferredSize(new Dimension(1100, 140));
 		pan3.setBackground(Color.gray);
-		tf = new JTextField("Welcome to Escape OS!");
+		pan3.add(new IButton("PrisonBG.jpg"));
+		tf = new JTextField();
 		tf.setPreferredSize(new Dimension(1100, 40));
 		tf.setEnabled(false);
 		tf.setDisabledTextColor(Color.YELLOW);
@@ -74,6 +80,9 @@ public class GameWindow extends JFrame{
 		pan1.setBackground(Color.BLACK);
 		pan1.add(pan4);
 		this.add(pan1);
+		
+		//animate the text at startup
+		animateText(tf, "Welcome to Escape OS!");
 		
 		//Menu
 		JMenu i1 = new JMenu("File");
@@ -121,6 +130,8 @@ public class GameWindow extends JFrame{
 		
 		this.setJMenuBar(mb);
 		
+		
+		
 		this.setVisible(true);
 		
 	}
@@ -128,18 +139,75 @@ public class GameWindow extends JFrame{
 	//new button class
 	private class IButton extends JButton{
 		
+		boolean clicked;
+		
 		public IButton(String filename){//place the name of the picture file in here, formatting is handled here.
 			
+			clicked = false;
 			this.setSize(140, 140);
 			this.setBackground(Color.gray);
 			Image img = (new ImageIcon(filename)).getImage();
 			Image newimg = img.getScaledInstance(100, 100,  java.awt.Image.SCALE_FAST);
 			this.setIcon(new ImageIcon(newimg));
 			this.setMargin(new Insets(0, 0, 0, 0));
+			this.setBorder(BorderFactory.createRaisedBevelBorder());
+
+			
+			this.addMouseListener(new MouseListener(){
+
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					
+					
+					
+				}
+
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseExited(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+					
+					
+				}
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					if(clicked){//deselect button
+						
+						IButton.this.setBorder(BorderFactory.createRaisedBevelBorder());
+						
+						clicked = false;
+					}else{//select button
+						
+						IButton.this.setBorder(BorderFactory.createLoweredBevelBorder());
+						//code for using the button here
+						
+						clicked = true;
+					
+						
+						
+						
+						
+						
+					}
+					
+				}
+				
+			});
 		}
 		
 	}
-	
+
 	//new label class
 	private class ILabel extends JLabel{
 		
@@ -159,6 +227,28 @@ public class GameWindow extends JFrame{
 			
 		}
 	}
+	
+	//animate text to look like its being typed
+	private void animateText(JTextField jtf, String tex){
+	     
+		charIndex = 0;
+
+		 Timer timer = new Timer(80, new ActionListener() {//set the timer to type a letter into the field than wait 80 ms
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 String labelText = jtf.getText();
+                 labelText += tex.charAt(charIndex);
+                 jtf.setText(labelText);
+                 charIndex++;
+                 if (charIndex >= tex.length()) {
+                     ((Timer)e.getSource()).stop();
+                 }
+             }
+         });
+         timer.start();
+	}
+	
+	
 	
 	
 
