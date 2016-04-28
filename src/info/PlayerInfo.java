e6371e5d5a3f608de7c191745a105acc44775205
SKThung <tychobraheproductions@yahoo.com>
2016-04-28 02:10:54 -0500

    Best version

--- src/info/PlayerInfo.java
+++ src/info/PlayerInfo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package info;
 
 import java.awt.Point;
@@ -62,50 +61,3 @@ public class PlayerInfo {
 		return gameProgression;
 	}
 }
-=======
-package info;
-
-import java.awt.Point;
-
-public class PlayerInfo {
-
-	/**
-	 * Array to keep track of inventory.
-	 * 0 indicates that the item is not yet gathered.
-	 * 1 indicates that the user has the item.
-	 * 2 indicates that the item is selected.
-	 * -2 indicates that the item was gathered, but then lost.
-	 */
-	private int[] inventory = {0,0,0,0,0,0};
-	
-	private Point lastClickPos;
-	
-	/**
-	 * Modifies the inventory item accordingly.
-	 * @param slot
-	 * @param mod
-	 */
-	public void modifyItem(int slot, ItemMod mod)
-	{
-		inventory[slot] = mod.Val();
-	}
-	
-	////////////////////////
-	//Getters and Setters://
-	//********************//
-	public int[] getInventory()
-	{
-		return inventory;
-	}
-	
-	public void setLastClickPos(Point p)
-	{
-		lastClickPos = p;
-	}
-	
-	public Point getLastClickPos()
-	{
-		return lastClickPos;
-	}
-}
->>>>>>> branch 'master' of https://github.com/euda8793/EscapeOSclass.git
